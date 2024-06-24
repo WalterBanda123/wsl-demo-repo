21298d8c765f3fd7ceb3a684b0ab2ea7eee0885f
Walter Banda <walterbanda98@gmail.com>
2024-06-24 14:48:16 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+const express =  require("express")
+const app = express()
+const port = process.env.PORT
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const {StatusCodes}  = require("http-status-codes")
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(cors("*"))
+app.use(express.json())
+
+
+app.get('/api/version-01/testing', async(request, response)=>{
+	try{
+		response.status(StatusCodes.OK).send("Testing was a success");
+	}catch(error){
+		response.send(error);
+	}}
+)
+
+
+
+app.listen(port , ()=>{
+	console.log(`App running on port ${port}`)
+})
+
